5392877f39fb1275955203ea20ab2b0dcf667e74
Matthew Finn <m.finn11@nuigalway.ie>
2016-09-25 23:41:11 +0100

    Back to better baseline

--- src/main/java/ie/nuigalway/mfinn/StudentRegistrationDriver/StudentRegistrationDriver.java
+++ src/main/java/ie/nuigalway/mfinn/StudentRegistrationDriver/StudentRegistrationDriver.java
@@ -7,24 +7,11 @@ import ie.nuigalway.mfinn.StudentRegistration.Student;
 public class StudentRegistrationDriver {
 	public static void main(String[] args) {
 
-		Course c1 = new Course("CS & IT", null, null, null, null);
+		Course c1 = new Course("CS & IT", null, null);
 		Student s1 = new Student("Matthew", "Finn", "22/09/1994", 22, "13480362", null);
-		Student s2 = new Student("Matthew", "Finn", "22/09/1994", 22, "13480362", null);
-		Student s3 = new Student("Matthew", "Finn", "22/09/1994", 22, "13480362", null);
-		Module m1 = new Module(null, null, null);
+		Student s2 = new Student("Mark", "Jones", "13/07/1995", 21, "13343567", null);
+		Module m1 = new Module("Software Engineering 3", "CT417");
 
-		System.out.println(s1.toString());
-		/*
-		 * m1.addStudent(s1); m1.addStudent(s2); m1.addStudent(s3);
-		 * c1.addModule(m1);
-		 *
-		 * c1.addStudent(s1); c1.addStudent(s2); c1.addStudent(s3);
-		 */
-		/*
-		 * ArrayList<Student> students = c1.getStudents(); Iterator<Student> it
-		 * = students.iterator(); while (it.hasNext()) {
-		 * System.out.println(it.next().toString()); }
-		 */
 	}
 
 }
