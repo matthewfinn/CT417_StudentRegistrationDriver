d04829f89006e8afbfd4a96ce137e546ce3a25a7
Matthew Finn <m.finn11@nuigalway.ie>
2016-09-26 17:55:06 +0100

    Updated driver class with most functionality finished

--- src/main/java/ie/nuigalway/mfinn/StudentRegistrationDriver/StudentRegistrationDriver.java
+++ src/main/java/ie/nuigalway/mfinn/StudentRegistrationDriver/StudentRegistrationDriver.java
@@ -1,5 +1,7 @@
 package ie.nuigalway.mfinn.StudentRegistrationDriver;
 
+import java.util.ArrayList;
+
 import org.joda.time.DateTime;
 
 import ie.nuigalway.mfinn.StudentRegistration.Course;
@@ -30,9 +32,26 @@ public class StudentRegistrationDriver {
 		Student s10 = new Student("Patrick", "Walsh", "20/3/1996", 20, "13495677", c1);
 
 
-		m1.addStudent(s1);m1.addStudent(s1);
-		System.out.println(c1.getStartDate());
-		System.out.println(c1.getEndDate());
+		//adds all declared students to each of the modules
+		m1.addStudent(s1);m1.addStudent(s2);m1.addStudent(s3);m1.addStudent(s4);m1.addStudent(s5);
+		m1.addStudent(s6);m1.addStudent(s7);m1.addStudent(s8);m1.addStudent(s9);m1.addStudent(s10);
+		m2.addStudent(s1);m2.addStudent(s2);m2.addStudent(s3);m2.addStudent(s4);m2.addStudent(s5);
+		m2.addStudent(s6);m2.addStudent(s7);m2.addStudent(s8);m2.addStudent(s9);m2.addStudent(s10);
+		m3.addStudent(s1);m3.addStudent(s2);m3.addStudent(s3);m3.addStudent(s4);m3.addStudent(s5);
+		m3.addStudent(s6);m3.addStudent(s7);m3.addStudent(s8);m3.addStudent(s9);m3.addStudent(s10);
+		m4.addStudent(s1);m4.addStudent(s2);m4.addStudent(s3);m4.addStudent(s4);m4.addStudent(s5);
+		m4.addStudent(s6);m4.addStudent(s7);m4.addStudent(s8);m4.addStudent(s9);m4.addStudent(s10);
+		m5.addStudent(s1);m5.addStudent(s2);m5.addStudent(s3);m5.addStudent(s4);m5.addStudent(s5);
+		m5.addStudent(s6);m5.addStudent(s7);m5.addStudent(s8);m5.addStudent(s9);m5.addStudent(s10);
+
+		//adding modules to course
+		c1.addModule(m1);c1.addModule(m2);c1.addModule(m3);c1.addModule(m4);c1.addModule(m5);
+
+		ArrayList<Student> s = c1.getStudents();
+		for(Student stu: s){
+			System.out.println("Student: " + stu.toString() + stu.getModules().toString() + "\n\n");
+		}
+
 
 	}
 
